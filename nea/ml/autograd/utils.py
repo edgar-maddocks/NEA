51cfa9e400d248e87c53af83a1779aad9dd86d82
edgar-maddocks <edgarmaddocks@outlook.com>
2024-08-17 21:21:01 +0100

    Implement CUDA functions

--- nea/ml/autograd/utils.py
+++ nea/ml/autograd/utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 import time as t
-import numba
-from numba import cuda
+from numba import cuda, jit
 
 from nea.ml.autograd import Tensor, Tensorable
 
@@ -39,6 +38,7 @@ def tensor_exp(a: Tensor) -> Tensor:
         Tensor: 
     """
     return a.exp()
+    
 
 def _np_cross_correlate(x: np.ndarray, k: np.ndarray) -> np.ndarray:
     """Performs cross correlation between two numpy arrays
@@ -47,8 +47,8 @@ def _np_cross_correlate(x: np.ndarray, k: np.ndarray) -> np.ndarray:
     otherwise a default cpu version.
 
     Args:
-        x (np.ndarray): input array
-        k (np.ndarray): filter/kernel
+        x (np.ndarray): input array - shape(n_samples, *, *)
+        k (np.ndarray): filter/kernel - shape(*, *)
 
     Returns:
         np.ndarray: result of cross-correlation between x and k
@@ -56,24 +56,32 @@ def _np_cross_correlate(x: np.ndarray, k: np.ndarray) -> np.ndarray:
     assert len(x.shape) == 3, "Input must be shape (n_samples, *, *)"
     assert len(k.shape) == 2, "Filter must be shape (*, *)"
 
-    x_depth, x_rows, x_cols = x.shape
-    k_rows, k_cols = k.shape
+    x_depth, _, _ = x.shape
 
     output = None
 
-    use_cuda = cuda.is_available() and x_depth > 100000000     
+    use_cuda = cuda.is_available()   
 
     if use_cuda:
-        print("CUDA DEVICE DETECTED TO USE")
-        print(cuda.detect())
-        output = _cuda_cross_correlate(x, k, output)
-    elif x_depth >= 10000:
+        # print("CUDA DEVICE DETECTED TO USE")
+        # print(cuda.detect())
+        output = _cuda_cross_correlate(x, k)
+    else:
         output = _jit_cpu_cross_correlate(x, k)
     
     return output
 
-@numba.jit(nopython=True, cache=True)
+@jit(nopython=True, cache=True)
 def _jit_cpu_cross_correlate(a: np.ndarray, b: np.ndarray) -> np.ndarray:
+    """Performs cross correlation between two numpy arrays - compiled
+
+    Args:
+        a (np.ndarray): 
+        b (np.ndarray): 
+
+    Returns:
+        np.ndarray: 
+    """
     a_depth, a_rows, a_cols = a.shape
     b_rows, b_cols = b.shape
 
@@ -92,23 +100,92 @@ def _jit_cpu_cross_correlate(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     
     return output
 
-# TODO: Implement CUDA functions -> https://numba.readthedocs.io/en/stable/cuda/kernels.html
 @cuda.jit(cache=True)
 def _jit_cuda_cross_correlate(a: np.ndarray, b: np.ndarray, output: np.ndarray):
-    # actually performs cross correlation
-    pass
+    """ Performs cross correlation on one thread for cuda
 
-def _cuda_cross_correlate(a: np.ndarray, b: np.ndarray, output: np.ndarray):
-    ## prepare blocks and grids and threads
-    pass
+    Args:
+        a (np.ndarray): 
+        b (np.ndarray): 
+        output (np.ndarray): 
+    """
+    # get position of thread
+    thread_idx, i, j = cuda.grid(3)
+    
+    n_samples, a_rows, a_cols = a.shape
+    b_rows, b_cols = b.shape
+    
+    if thread_idx < n_samples and i < (a_rows - b_rows + 1) and j < (a_cols - b_cols + 1): # check thread is within bounds
+        acc = 0.0
+        for di in range(b_rows):
+            for dj in range(b_cols):
+                acc += a[thread_idx, i + di, j + dj] * b[di, dj]
+        output[thread_idx, i, j] = acc
 
-if __name__ == "__main__":
-    a = np.random.randn(100000, 32, 32)
-    b = np.random.randn(6, 6)
+def _cuda_cross_correlate(a: np.ndarray, b: np.ndarray):
+    """Prepares the shape of blocks and grids, as well as the output array
+    and then calls the cuda compiled function
+
+    Args:
+        a (np.ndarray): _description_
+        b (np.ndarray): _description_
+    """
+    # prepare blocks and grids and threads
+    n_samples, a_rows, a_cols = a.shape
+    b_rows, b_cols = b.shape
+
+    output_shape = (n_samples, a_rows - b_rows + 1, a_cols - b_cols + 1)
+    output = np.zeros(output_shape, dtype=np.float32)
+
+    # bpg calculated according to numba docs
+    threads_per_block = (16, 16, 1)
+    blocks_per_grid = (n_samples, (a_rows - b_rows + 1 + threads_per_block[0] - 1) // threads_per_block[0],
+                    (a_cols - b_cols + 1 + threads_per_block[1] - 1) // threads_per_block[1])
+
+    _jit_cuda_cross_correlate[blocks_per_grid, threads_per_block](a, b, output)
+
+    return output
+    
+
+
+def __compare_cpu_cuda():
+    number_samples = 10000
+    H_a, W_a = 8, 8
+    H_b, W_b = 4, 4
+
+    a = np.random.random((number_samples, H_a, W_a)).astype(np.float32)
+    b = np.random.random((H_b, W_b)).astype(np.float32)
+
+    start = t.time()
+
+    result = _cuda_cross_correlate(a, b)
+
+    print("TIME TAKEN CUDA: ", t.time() - start)
+    print(result.shape)
+
+    start = t.time()
+
+    result = _jit_cpu_cross_correlate(a, b)
+
+    print("TIME TAKEN CPU: ", t.time() - start)
+    print(result.shape)
+
+def __cross_correlate():
+    number_samples = 1000000
+    H_a, W_a = 8, 8
+    H_b, W_b = 4, 4
+
+    a = np.random.random((number_samples, H_a, W_a)).astype(np.float32)
+    b = np.random.random((H_b, W_b)).astype(np.float32)
 
     start = t.time()
 
     result = _np_cross_correlate(a, b)
 
     print("TIME TAKEN: ", t.time() - start)
-    print("RESULT SHAPE: ", result.shape)
+    print(result.shape)
+
+
+"""if __name__ == "__main__":
+    __compare_cpu_cuda()
+    __cross_correlate()"""
