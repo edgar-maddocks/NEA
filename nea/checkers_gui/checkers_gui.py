2593937ef2563dd97dd327681ba6b96fb14d46b1
edgar-maddocks <edgarmaddocks@outlook.com>
2024-09-07 19:33:40 +0100

    Work on GUI selection

--- nea/checkers_gui/checkers_gui.py
+++ nea/checkers_gui/checkers_gui.py
@@ -1,6 +1,16 @@
 from nea.ConsoleCheckers import CheckersGame
-from nea.ConsoleCheckers.consts import SIZE as BOARD_SIZE, BLACK_K, BLACK_R, WHITE_K, WHITE_R, EMPTY
+from nea.ConsoleCheckers.consts import (SIZE as BOARD_SIZE, 
+                                        BLACK_K, 
+                                        BLACK_R, 
+                                        WHITE_K, 
+                                        WHITE_R, 
+                                        EMPTY, 
+                                        WHITE, 
+                                        WHITES, 
+                                        BLACK, 
+                                        BLACKS)
 from nea.checkers_gui.consts import *
+from nea.checkers_gui.helpers import *
 
 import pygame
 
@@ -10,15 +20,16 @@ class CheckersGUI(CheckersGame):
     """
     def __init__(self, game: CheckersGame) -> None:
         self.game = game
+        self.piece_selected = None
 
     def draw(self, screen: pygame.Surface):
         for row in range(8):
             if row % 2 == 0:
                 for x in range(0, DISPLAY.SCREEN_SIZE, DISPLAY.SQUARE_SIZE*2):
-                    pygame.draw.rect(screen, COLOURS.BLACK, pygame.Rect(x, row*DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE))
+                    pygame.draw.rect(screen, COLOURS.WHITE, pygame.Rect(x, row*DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE))
             else:
                 for x in range(DISPLAY.SQUARE_SIZE, DISPLAY.SCREEN_SIZE, DISPLAY.SQUARE_SIZE*2):
-                    pygame.draw.rect(screen, COLOURS.BLACK, pygame.Rect(x, row*DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE))
+                    pygame.draw.rect(screen, COLOURS.WHITE, pygame.Rect(x, row*DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE, DISPLAY.SQUARE_SIZE))
         for y in range(BOARD_SIZE):
             for x in range(BOARD_SIZE):
                 piece = self.game.board[x, y]
@@ -26,10 +37,33 @@ class CheckersGUI(CheckersGame):
                     self._draw_piece(screen, piece=piece, x=x, y=y)
 
 
-    def _draw_piece(self, screen: pygame, piece: int, x: int, y: int) -> None:
+    def _draw_piece(self, screen: pygame.Surface, piece: int, x: int, y: int) -> None:
         if piece == BLACK_R:
-            black_r = pygame.image.load("nea/checkers_gui/media/images/black_r.png")
-            pygame.draw.circle(screen, COLOURS.BROWN, center=(y*DISPLAY.SQUARE_SIZE + DISPLAY.SQUARE_SIZE/2, x*DISPLAY.SQUARE_SIZE + DISPLAY.SQUARE_SIZE/2), radius=DISPLAY.PIECE_RADIUS)
+            black_r = pygame.image.load("nea/checkers_gui/media/images/black_r.png").convert_alpha()
+            screen.blit(black_r, (y*DISPLAY.SQUARE_SIZE, x*DISPLAY.SQUARE_SIZE))
+        elif piece == BLACK_K:
+            black_k = pygame.image.load("nea/checkers_gui/media/images/black_k.png").convert_alpha()
+            screen.blit(black_k, (y*DISPLAY.SQUARE_SIZE, x*DISPLAY.SQUARE_SIZE))
+        elif piece == WHITE_R:
+            white_r = pygame.image.load("nea/checkers_gui/media/images/white_r.png").convert_alpha()
+            screen.blit(white_r, (y*DISPLAY.SQUARE_SIZE, x*DISPLAY.SQUARE_SIZE))
+        elif piece == WHITE_K:
+            white_k = pygame.image.load("nea/checkers_gui/media/images/white_k.png").convert_alpha()
+            screen.blit(white_k, (y*DISPLAY.SQUARE_SIZE, x*DISPLAY.SQUARE_SIZE))
+
+    def click(self, mouse_pos: tuple[int, int]) -> None:
+        mouse_x, mouse_y = mouse_pos
+        row, col = get_row_selected(mouse_y=mouse_y), get_col_selected(mouse_x=mouse_x)
+
+        if self.piece_selected is None:
+            if self.game.player == WHITE:
+                if self.game.board[row, col] in WHITES:
+                    self.piece_selected = (row, col)
+            elif self.game.player == BLACK:
+                if self.game.board[row, col] in BLACKS:
+                    self.piece_selected = (row, col)
+        else:
+            pass
 
 
 def game_loop(game: CheckersGame) -> None:
@@ -45,8 +79,10 @@ def game_loop(game: CheckersGame) -> None:
 
             if e.type == pygame.QUIT:
                 game_in_progress = False
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                game.click(pygame.mouse.get_pos())
 
-        screen.fill(COLOURS.WHITE)
+        screen.fill(COLOURS.BLACK)
         gui.draw(screen)
         pygame.display.flip()
 
---
+++ nea/checkers_gui/helpers.py
@@ -0,0 +1,7 @@
+from nea.checkers_gui.consts import *
+
+def get_row_selected(mouse_y: int) -> int:
+    return mouse_y % DISPLAY.SQUARE_SIZE
+
+def get_col_selected(mouse_x: int) -> int:
+    return mouse_x % DISPLAY.SQUARE_SIZE
