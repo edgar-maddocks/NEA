0b2f603cdc79ab2097e32ad72c4cfaa32ed4e9c1
edgar-maddocks <edgarmaddocks@outlook.com>
2024-09-21 10:02:17 +0100

    Create Full AlphaModel
    - Fix res layer to stop overflow
    - Change transpose property of tensor
    - Fix MatMul to allow 3D MatMul
    - Create Module List

---
+++ nea/agent/__init__.py
@@ -0,0 +1,3 @@
+from nea.agent.network import AlphaModel
+
+__all__ = ["AlphaModel"]
--- nea/agent/network.py
+++ nea/agent/network.py
@@ -1,41 +1,103 @@
 from copy import deepcopy
-import time as t
+import numpy as np
 
 from nea.ml import nn
 from nea.ml.autograd import Tensor, Tensorable
-from nea.console_checkers import CheckersGame
 
 
 class ResidualLayer(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_hidden_conv: int) -> None:
         super().__init__()
         self.Conv1 = nn.Conv2D(
-            (15, 8, 8), kernel_size=3, n_kernels=128, padding=1, padding_value=0
+            (num_hidden_conv, 8, 8),
+            kernel_size=3,
+            n_kernels=num_hidden_conv,
+            padding=1,
+            padding_value=0,
         )
         self.ReLU1 = nn.ReLU()
         self.Conv2 = nn.Conv2D(
-            (128, 8, 8), kernel_size=3, n_kernels=15, padding=1, padding_value=0
+            (num_hidden_conv, 8, 8),
+            kernel_size=3,
+            n_kernels=num_hidden_conv,
+            padding=1,
+            padding_value=0,
         )
         self.ReLU2 = nn.ReLU()
 
     def forward(self, x: Tensorable) -> Tensor:
+        """Input Shape of (128, 8, 8)
+
+        Output Shape of (128, 8, 8)
+
+        Args:
+            x (Tensorable): input tensor
+
+        Returns:
+            Tensor:
+        """
         original_input = deepcopy(x)
         x = self.Conv1(x)
         x = self.ReLU1(x)
         x = self.Conv2(x)
         x += original_input
         x = self.ReLU2(x)
+        x /= np.max(x.data)
+        return x
+
+
+class ConvolutionalLayer(nn.Module):
+    """This is only used as the starting block
+
+    Args:
+        nn (_type_):
+    """
+
+    def __init__(self, num_hidden_conv: int) -> None:
+        super().__init__()
+        self.Conv1 = nn.Conv2D(
+            (15, 8, 8),
+            n_kernels=num_hidden_conv,
+            kernel_size=3,
+            padding=1,
+            padding_value=0,
+        )
+        self.ReLU = nn.ReLU()
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Input shape of (15, 8, 8)
+
+        Output shape of (128, 8, 8)
+
+        Args:
+            x (Tensor): input tensor
+
+        Returns:
+            Tensor:
+        """
+        x = self.Conv1(x)
+        x = self.ReLU(x)
         return x
 
 
 class PolicyHead(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_hidden_conv: int) -> None:
         super().__init__()
-        self.Conv1 = nn.Conv2D((15, 8, 8), kernel_size=1, n_kernels=8)
+        self.Conv1 = nn.Conv2D((num_hidden_conv, 8, 8), kernel_size=1, n_kernels=8)
         self.ReLU1 = nn.ReLU()
         self.Softmax = nn.Softmax()
 
     def forward(self, x: Tensor) -> Tensor:
+        """Input shape of (128, 8, 8)
+
+        Output shape of (8, 8, 8)
+
+        Args:
+            x (Tensor): input tensor
+
+        Returns:
+            Tensor:
+        """
         x = self.Conv1(x)
         x = self.ReLU1(x)
         x = self.Softmax(x)
@@ -43,9 +105,9 @@ class PolicyHead(nn.Module):
 
 
 class ValueHead(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, num_hidden_conv: int) -> None:
         super().__init__()
-        self.Conv1 = nn.Conv2D((15, 8, 8), kernel_size=1, n_kernels=8)
+        self.Conv1 = nn.Conv2D((num_hidden_conv, 8, 8), kernel_size=1, n_kernels=8)
         self.ReLU1 = nn.ReLU()
         self.Reshape = nn.Reshape((1, 8 * 8 * 8))
         self.Dense1 = nn.Dense(8 * 8 * 8, 256)
@@ -54,6 +116,16 @@ class ValueHead(nn.Module):
         self.Tanh = nn.Tanh()
 
     def forward(self, x: Tensor) -> Tensor:
+        """Input shape of (128, 8, 8)
+
+        Output is scalar [-1, 1]
+
+        Args:
+            x (Tensor): input tensor
+
+        Returns:
+            Tensor:
+        """
         x = self.Conv1(x)
         x = self.ReLU1(x)
         x = self.Reshape(x)
@@ -64,48 +136,37 @@ class ValueHead(nn.Module):
         return x
 
 
-if __name__ == "__main__":
-    res_layer = ResidualLayer()
-    value_head = ValueHead()
-    policy_head = PolicyHead()
+class AlphaModel(nn.Module):
+    def __init__(self, n_res_layers: int = 10, num_hidden_conv: int = 64) -> None:
+        super().__init__()
+        self.first_layer = ConvolutionalLayer(num_hidden_conv=num_hidden_conv)
+        self.res_layers = nn.ModuleList(
+            [
+                ResidualLayer(num_hidden_conv=num_hidden_conv)
+                for r in range(n_res_layers)
+            ]
+        )
+        self.policy_head = PolicyHead(num_hidden_conv=num_hidden_conv)
+        self.value_head = ValueHead(num_hidden_conv=num_hidden_conv)
 
-    samples = []
-    for i in range(15):
-        samples.append(CheckersGame().board)
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+        """Input shape (15, 8, 8)
 
-    input = Tensor(samples, requires_grad=True)
+        Output of tuple (policy, value)
 
-    print("#######################################################")
+        Policy has shape (8, 8, 8)
 
-    print("STARTING TIMINGS FOR RESIDUAL LAYER")
-    start = t.time()
-    y = res_layer(input)
-    print("FORWARD PASS TOOK: ", t.time() - start, "seconds")
+        Value is scalar
 
-    start = t.time()
-    y.backward()
-    print("BACKWARD PASS TOOK: ", t.time() - start, "seconds")
+        Args:
+            x (Tensor): input tensor
 
-    print("#######################################################")
-
-    print("STARTING TIMINGS FOR VALUE HEAD")
-    start = t.time()
-    y = value_head(input)
-    print("FORWARD PASS TOOK: ", t.time() - start, "seconds")
-
-    start = t.time()
-    y.backward()
-    print("BACKWARD PASS TOOK: ", t.time() - start, "seconds")
-
-    print("#######################################################")
-
-    print("STARTING TIMINGS FOR POLICY HEAD")
-    start = t.time()
-    y = policy_head(input)
-    print("FORWARD PASS TOOK: ", t.time() - start, "seconds")
-
-    start = t.time()
-    y.backward()
-    print("BACKWARD PASS TOOK: ", t.time() - start, "seconds")
-
-    print("#######################################################")
+        Returns:
+            tuple[Tensor, Tensor]: (policy, value)
+        """
+        x = self.first_layer(x)
+        for res in self.res_layers:
+            x = res(x)
+        pol = self.policy_head(x)
+        val = self.value_head(x)
+        return pol, val
