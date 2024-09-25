4d79f8cc206b42831dbea3ec1fbdca2957c7c1ee
edgar-maddocks <edgarmaddocks@outlook.com>
2024-09-25 23:49:31 +0100

    Finish off first AlphaZero train

--- nea/agent/agent.py
+++ nea/agent/agent.py
@@ -1,48 +1,126 @@
 import numpy as np
 from collections import deque
+import itertools
+import random
 
-from nea.ml.nn import Optimizer, SGD
-from nea.ml.autograd import to_tensor
+from nea.ml.nn import Optimizer, SGD, AlphaLoss
+from nea.ml.autograd import Tensor
 from nea.mcts import AlphaMCTS, AlphaNode
 from nea.network import AlphaModel
 from nea.console_checkers import CheckersGame
-from nea.console_checkers.consts import WHITE, BLACK
-from nea.agent.dataclasses import SAP, SPV
+from nea.agent.memory_types import SAP, SPV
 from nea.agent.consts import ACTION_SPACE
 
 
 class AlphaZero:
     def __init__(
         self,
-        model: AlphaModel = AlphaModel(),
-        optimizer: Optimizer = SGD(),
-        game: CheckersGame = None,
-        **kwargs,
+        optimizer: Optimizer = None,
+        mcts_epochs: int = 5,
+        max_examples: int = 1000,
+        nn_epochs: int = 100,
+        n_compare_games: int = 10,
+        eec: float = 1.41,
+        n_mcts_searches: int = 300,
+        replace_win_pct_threshold: int = 59,
     ) -> None:
-        self.prev_model = model
-        self.new_model = model
+        self.prev_model = None
+        self.new_model = None
+        if optimizer is None:
+            optimizer = SGD
         self.optimizer = optimizer
-        self.game = game
-        self.kwargs = kwargs
+        self.hyperparams = {
+            "mcts_epochs": mcts_epochs,
+            "max_examples": max_examples,
+            "nn_epochs": nn_epochs,
+            "n_compare_games": n_compare_games,
+            "eec": eec,
+            "n_mcts_searches": n_mcts_searches,
+            "replace_win_pct_threshold": replace_win_pct_threshold,
+        }
 
-        self.mcts = AlphaMCTS(
-            model=self.model,
-            eec=self.kwargs["eec"],
-            n_searches=self.kwargs["n_searches"],
-        )
+        self.loss = AlphaLoss()
 
-    def train(self) -> None:
-        for mcts_epoch in self.kwargs["mcts_epochs"]:
-            example_games = deque(maxlen=self.kwargs["max_examples"])
+    def train(self, initialModel: AlphaModel) -> None:
+        self.prev_model = initialModel
+        if self.new_model is None:
+            self.new_model = initialModel
 
-            for example in range(int(self.kwargs["max_examples"])):
+        for mcts_epoch in self.hyperparams["mcts_epochs"]:
+            example_games = deque(maxlen=self.hyperparams["max_examples"])[[], []]
+
+            for example in range(int(self.hyperparams["max_examples"])):
                 game_saps, reward, player = self._get_example_saps()
                 game_spvs = self._convert_saps_to_spvs(game_saps, player, reward)
 
-                example_games.append(game_spvs)
+                example_games.append(*game_spvs)
+
+            for epoch in range(int(self.hyperparams["nn_epochs"])):
+                self._train_nn(example_games)
+
+            new_nn_wins = 0
+            prev_nn_wins = 0
+
+            for compare_game in range(int(self.hyperparams["n_compare_games"])):
+                game = CheckersGame()
+
+                prev_mcts = AlphaMCTS(
+                    self.prev_model,
+                    eec=self.hyperparams["eec"],
+                    n_searches=self.hyperparams["n_mcts_searches"],
+                )
+                new_mcts = AlphaMCTS(
+                    self.new_model,
+                    eec=self.hyperparams["eec"],
+                    n_searches=self.hyperparams["n_mcts_searches"],
+                )
+
+                prior_states = deque(maxlen=5)
+
+                done = False
+                prev_nn_player = np.random.choice(["white", "black"], 1)
+
+                while not done:
+                    prior_states.append(game.board)
+
+                    if game.player == prev_nn_player:
+                        prev_mcts.alpha_build_tree(game, prior_states)
+                        action = prev_mcts.get_action()
+
+                        valid, _, done, reward = game.step(action)
+                        if not valid:
+                            print("TRIED TO MAKE INVALID MOVE")
+                        if done and reward == 1:
+                            prev_nn_wins += 1
+                        elif done and reward == -1:
+                            new_nn_wins += 1
+
+                    else:
+                        new_mcts.alpha_build_tree(game, prior_states)
+                        action = new_mcts.get_action()
+
+                        valid, _, done, reward = game.step(action)
+                        if not valid:
+                            print("TRIED TO MAKE INVALID MOVE")
+                        if done and reward == 1:
+                            new_nn_wins += 1
+                        elif done and reward == -1:
+                            prev_nn_wins += 1
+
+            new_nn_win_pct = (new_nn_wins / prev_nn_wins) * 100
+            if new_nn_win_pct > self.hyperparams["replace_win_pct_threshold"]:
+                self.prev_model = self.new_model
+
+        return self.prev_model
 
     def _get_example_saps(self) -> tuple[deque[SAP], float, str]:
         game = CheckersGame()
+
+        mcts = AlphaMCTS(
+            self.prev_model,
+            eec=self.hyperparams["eec"],
+            n_searches=self.hyperparams["n_searches"],
+        )
 
         game_saps = deque()
         prior_states = deque(maxlen=5)
@@ -51,15 +129,15 @@ class AlphaZero:
         while not done:
             prior_states.append(game.board)
 
-            self.mcts.alpha_build_tree(game, prior_states)
-            action_probs = self.mcts.get_action_probs()
+            mcts.alpha_build_tree(game, prior_states)
+            action_probs = mcts.get_action_probs()
 
             if len(prior_states) < 4:
                 action = AlphaNode._convert_action_idx_to_action_game(
                     np.random.choice(ACTION_SPACE)
                 )
             else:
-                action = self.mcts.convert_probs_to_action(action_probs)
+                action = mcts.convert_probs_to_action(action_probs)
 
             game_saps.append(SAP(game.board, action_probs, game.player))
 
@@ -67,5 +145,53 @@ class AlphaZero:
 
         return game_saps, reward, game.player
 
-    def _convert_saps_to_spvs(self, game_saps: deque[SAP]) -> deque[SPV]:
-        raise NotImplementedError
+    def _convert_saps_to_spvs(
+        self, game_saps: deque[SAP], player: str, reward: float
+    ) -> deque[SPV]:
+        len_game_saps = len(game_saps)
+        game_spvs = deque(maxlen=len_game_saps)
+
+        for idx, item in enumerate(game_saps):
+            value = reward if item.player == player else reward * -1
+            game_spvs.append(SPV(item.state, item.mcts_action_probs, value))
+
+        return game_spvs
+
+    def _train_nn(self, example_games: deque[SPV]) -> deque[SPV]:
+        random.shuffle(example_games)
+        len_example_games = len(example_games)
+        for batch_idx in range(0, len_example_games, self.hyperparams["batch_size"]):
+            end_idx = min(
+                len_example_games - 1, batch_idx + self.hyperparams["batch_size"]
+            )
+            batch = list(itertools.islice(example_games, batch_idx, end_idx))
+
+            past_states = []
+            for i in range(0, len(batch)):
+                spv = batch[i]
+
+                if len(past_states) > 5:
+                    past_states.pop(0)
+
+                past_states.append(spv.state)
+
+                if i > 5:
+                    state, mcts_probs, true_value = (
+                        Tensor(past_states, requires_grad=True),
+                        Tensor(spv.mcts_action_probs, requires_grad=True),
+                        Tensor(spv.true_value),
+                    )
+
+                    nn_policy, nn_value = self.new_model(state)
+
+                    loss = self.loss(true_value, nn_value, mcts_probs, nn_policy)
+
+                    optimizer = self.optimizer(self.new_model.params)
+
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+
+if __name__ == "__main__":
+    alphazero = AlphaZero(SGD)
