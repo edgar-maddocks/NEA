e638a24ade3b4e6255003a60872983e0a17727b8
edgar-maddocks <edgarmaddocks@outlook.com>
2024-12-04 13:20:21 +0000

    Refactor

--- nea/ml/autograd/__init__.py
+++ nea/ml/autograd/__init__.py
@@ -1,6 +1,6 @@
 from .tensor import Tensor, to_tensor, TensorFunction, no_grad
 from .consts import Tensorable
-from .utils import tensor_mean, tensor_sum, tensor_exp
+from .helpers import tensor_mean, tensor_sum, tensor_exp
 
 __all__ = [
     "Tensor",
